7df5f055a4371ef0edf4e5bdafa8cd7844fea5a0
ppAndrzejNiczyporuk <pp.Andrzej.Niczyporuk@gmail.com>
2019-04-26 13:57:13 +0200

    Wykorzystanie SWXMLHash Wyświetlenie zadarzeń Zamiana  kodów na komunikaty

--- tt/CodeList.swift
+++ tt/CodeList.swift
@@ -7,9 +7,121 @@
 //
 
 import Foundation
+import SWXMLHash
+
 var komunikatStatus:[String:String] =
     ["0":"Ok",
      "-1":"zapytanie o zbyt dużą liczbę przesyłek",
      "-2":"brak uprawnień do sprawdzania wielu przesyłek",
      "-3":"daty podane w wywołaniu są błędne",
      "-99":"inny błąd"]
+
+var przesylkaStatus:[String:String] =
+    ["0":"Ok",
+     "1":"są inne przesyłki o takim numerze",
+     "2":"przesyłka o podanym numerze jest w systemie, ale nie ma zdarzeń w podanym okresie",
+     "-1":"w systemie nie ma przesyłki o takim numerze.",
+     "-2":"podany numer przesyłki jest błędny",
+     "-99":"inny błąd"
+]
+var  kodRodzajPrzes:[String:String] =
+    ["AA"    :    "Paczka pocztowa Atestowa",
+     "B"    :    "Pocztex Kurier 48",
+     "BPR"    :    "Przesyłka biznesowa PLUS",
+     "CCRI"    :    "CCRI (IBRS)",
+     "CK"    :    "Cekogram",
+     "DA"    :    "Druk adresowy",
+     "DB"    :    "Druk bezadresowy",
+     "E"    :    "EMS zwykly",
+     "E05"    :    "EMS 0,5 Kilo Pack",
+     "E1"    :    "EMS Kilo Pack",
+     "ED"    :    "EMS Document Pack",
+     "EF1"    :    "EMS Full Pack 1",
+     "EF2"    :    "EMS Full Pack 2",
+     "EK"    :    "Paczka Mini",
+     "EL"    :    "E-Przesyłka list",
+     "ELB"    :    "E-Przesyłka list pobranie",
+     "EM"    :    "EMS Maxi Pack 50",
+     "EM_"    :    "EMS Towary",
+     "EMS"    :    "EMS - Przesyłka kurierska zagraniczna",
+     "EMS1"    :    "EMS Kilo Pack",
+     "EMSD"    :    "EMS Document Pack",
+     "EP"    :    "E-Przesyłka paczka",
+     "EPB"    :    "E-Przesyłka paczka pobranie",
+     "EUKPR"    :    "Express Ukraina",
+     "EXP"    :    "GLOBAL Expres",
+     "G"    :    "Przesyłka gabarytowa ekonomiczna",
+     "GPR"    :    "Przesyłka gabarytowa priorytetowa",
+     "HPR"    :    "Przesyłka hybrydowa rejestrowana",
+     "K"    :    "List zwykły kartowany ekonomiczny",
+     "KPR"    :    "List zwykły kartowany priorytetowy",
+     "L"    :    "List zwykły ekonomiczny",
+     "LB"    :    "Przesyłka pobraniowa ekonomiczna list",
+     "LBPR"    :    "Przesyłka pobraniowa priorytetowa list",
+     "LD"    :    "List dworcowy",
+     "LPR"    :    "List zwykły priorytetowy",
+     "LW"    :    "List wartościowy ekonomiczny",
+     "LWB"    :    "E-Przesyłka list pobranie",
+     "LWPR"    :    "List wartościowy priorytetowy",
+     "LWS"    :    "E-Przesyłka list",
+     "M"    :    "Worek M",
+     "MEK"    :    "Worek specjalny M ekonomiczny",
+     "MP"    :    "Multi Paczka Ekonomiczna",
+     "MPPR"    :    "Multi Paczka Priorytetowa",
+     "MPR"    :    "Worek M priorytetowy",
+     "MR"    :    "Worek M polecony ekonomiczny",
+     "MREK"    :    "Worek polecony M ekonomiczny",
+     "MRPR"    :    "Worek M polecony priorytetowy",
+     "N"    :    "Nieokreślony typ",
+     "NZNP"    :    "Przesyłka nierejestrowana z numerem",
+     "NZNPR"    :    "Przesyłka nierejestrowana z numerem priorytetowa",
+     "P"    :    "Paczka pocztowa ekonomiczna",
+     "PAK"    :    "Pakiet bez karty",
+     "PB"    :    "Przesyłka pobraniowa ekonomiczna paczka",
+     "PBPR"    :    "Przesyłka pobraniowa priorytetowa paczka",
+     "PD"    :    "Przesyłka dworcowa",
+     "PEK"    :    "Paczka pocztowa ekonomiczna",
+     "PL"    :    "Paleta",
+     "PM"    :    "paczka mała zwykła",
+     "PMW"    :    "paczka mała wartościowa",
+     "PP"    :    "POCZTEX PROCEDURA",
+     "PP1"    :    "POCZTEX PROCEDURA powrotna do DOKu",
+     "PP2"    :    "POCZTEX PROCEDURA powrotna do nadawcy",
+     "PPL"    :    "Przesyłka paletowa",
+     "PPLUS"    :    "Paczka+",
+     "PPR"    :    "Paczka pocztowa priorytetowa",
+     "PRZPO"    :    "Przekaz pocztowy",
+     "PW"    :    "Paczka wartościowa ekonomiczna",
+     "PWB"    :    "E-Przesyłka paczka pobranie",
+     "PWPR"    :    "Paczka wartościowa priorytetowa",
+     "PWS"    :    "E-Przesyłka paczka",
+     "PX"    :    "Pocztex - Przesyłka kurierska krajowa",
+     "PXF1"    :    "Pocztex Full Pack 1",
+     "PXF2"    :    "Pocztex Full Pack 2",
+     "PXN"    :    "Pocztex - Przesyłka kurierska krajowa",
+     "PXPB"    :    "Pocztex pobranie",
+     "R"    :    "List polecony ekonomiczny",
+     "RD"    :    "List polecony dworcowy",
+     "RPR"    :    "List polecony priorytetowy",
+     "S"    :    "Przesyłka umowna monitorowana",
+     "SPR"    :    "Przesyłka listowa monitorowana",
+     "UK"    :    "Usługa Kurierska",
+     "UKPR"    :    "UKPR",
+     "UP"    :    "Usługa Pocztowa",
+     "WP"    :    "Przesyłka wielopaczkowa",
+     "XXXX"    :    "PACZKA"
+]
+
+struct Zdarzenie: XMLIndexerDeserializable {
+    let jednostkaNazwa: String
+    let czas: String
+    let nazwa: String
+    
+    static func deserialize(_ node: XMLIndexer) throws -> Zdarzenie {
+        return try Zdarzenie(
+            jednostkaNazwa: node["ax21:jednostka"]["ax21:nazwa"].value(),
+            czas: node["ax21:czas"].value(),
+            nazwa: node["ax21:nazwa"].value()
+        )
+    }
+}
--- tt/ViewController.swift
+++ tt/ViewController.swift
@@ -7,18 +7,30 @@
 //
 
 import UIKit
+import SWXMLHash
 
-class ViewController: UIViewController, NSURLConnectionDelegate, NSURLConnectionDataDelegate, XMLParserDelegate {
+
+class ViewController: UIViewController, NSURLConnectionDelegate, NSURLConnectionDataDelegate, UITableViewDataSource {
 
     
+
+    
+    @IBOutlet weak var txtStatus: UITextField!
+    @IBOutlet weak var labStatus: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var labRodzajPrzes: UILabel!
+    @IBOutlet weak var txtRodzajPrzes: UITextField!
     @IBOutlet weak var txtParcel: UITextField!
     var status = "???"
     var mutableData:NSMutableData = NSMutableData()
     var currentElementName:String = ""
     let komStatus = komunikatStatus
+    let pStatus = przesylkaStatus
+    let kRP = kodRodzajPrzes
+    var zdarzenia : [Zdarzenie]? = nil
     
     
-    @IBAction func actrionCheck(_ sender: Any) {
+    @IBAction func actionCheck(_ sender: Any) {
         let numOfParcel = txtParcel.text
         let soapMessage = "<soapenv:Envelope xmlns:soapenv='http://schemas.xmlsoap.org/soap/envelope/' xmlns:sled='http://sledzenie.pocztapolska.pl'> <soapenv:Header> <wsse:Security soapenv:mustUnderstand='1' xmlns:wsse='http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd'> <wsse:UsernameToken wsu:Id='UsernameToken-2' xmlns:wsu='http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd'> <wsse:Username>sledzeniepp</wsse:Username> <wsse:Password Type='http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-username-token-profile-1.0#PasswordText'>PPSA</wsse:Password> <wsse:Nonce EncodingType='http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-soap-message-security-1.0#Base64Binary'>X41PkdzntfgpowZsKegMFg==</wsse:Nonce> <wsu:Created>2011-12-08T07:59:28.656Z</wsu:Created> </wsse:UsernameToken></wsse:Security> </soapenv:Header> <soapenv:Body> <sled:sprawdzPrzesylke> <sled:numer>" + (numOfParcel!) + "</sled:numer> </sled:sprawdzPrzesylke> </soapenv:Body> </soapenv:Envelope>"
         //print(soapMessage)
@@ -42,11 +54,28 @@ class ViewController: UIViewController, NSURLConnectionDelegate, NSURLConnection
     }
     func connectionDidFinishLoading(_ connection: NSURLConnection) {
         // tu będzie cała magia <#code#>
-        let xmlParser = XMLParser(data: mutableData as Data)
-        xmlParser.delegate = self
-        xmlParser.parse()
-        xmlParser.shouldResolveExternalEntities = true
-        showToast(message: komStatus[status]!)
+//        let xmlParser = XMLParser(data: mutableData as Data)
+//        xmlParser.delegate = self
+//        xmlParser.parse()
+//        xmlParser.shouldResolveExternalEntities = true
+//        showToast(message: komStatus[status]!)
+        let xml = SWXMLHash.parse(mutableData as Data)
+        self.status = xml["soapenv:Envelope"]["soapenv:Body"]["ns:sprawdzPrzesylkeResponse"]["ns:return"]["ax21:status"].element!.text
+        let intStatus = Int(self.status)
+        if intStatus == 0
+        {
+            txtRodzajPrzes.text = kRP[xml["soapenv:Envelope"]["soapenv:Body"]["ns:sprawdzPrzesylkeResponse"]["ns:return"]["ax21:danePrzesylki"]["ax21:kodRodzPrzes"].element!.text]
+            self.zdarzenia = try! xml["soapenv:Envelope"]["soapenv:Body"]["ns:sprawdzPrzesylkeResponse"]["ns:return"]["ax21:danePrzesylki"]["ax21:zdarzenia"]["ax21:zdarzenie"].value()
+            DispatchQueue.main.async { self.tableView.reloadData() }
+            IBOutletOn()
+            showToast(message: "Status Ok")
+        }
+        else
+        {
+            IBOutletOff()
+            txtStatus.text = pStatus[self.status]
+            showToast(message: "Status Bad")
+        }
     }
     func connection(_ connection: NSURLConnection, didFailWithError error: Error) {
         let txtSentence = "connection error = \(error)"
@@ -59,20 +88,51 @@ class ViewController: UIViewController, NSURLConnectionDelegate, NSURLConnection
     func connection(_ connection: NSURLConnection, didReceive data: Data) {
         mutableData.append(data)
     }
-    
-    func parser(_ parser: XMLParser, didStartElement elementName: String, namespaceURI: String?, qualifiedName qName: String?, attributes attributeDict: [String : String]) {	
-        currentElementName = elementName
-    }
-    func parser(_ parser: XMLParser, foundCharacters string: String) {
-        if(currentElementName == "ax21:status")
-        {
-        status = string
-        }
-    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
         // Do any additional setup after loading the view.
     }
+    func IBOutletOn()
+    {
+        //show list & hide status
+        labStatus.isHidden = true
+        txtStatus.isHidden = true
+        tableView.isHidden = false
+        labRodzajPrzes.isHidden = false
+        txtRodzajPrzes.isHidden = false
+    }
+    
+    func IBOutletOff()
+    {
+        //hide list & show status
+        labStatus.isHidden = false
+        txtStatus.isHidden = false
+        tableView.isHidden = true
+        labRodzajPrzes.isHidden = true
+        txtRodzajPrzes.isHidden = true
+        
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if zdarzenia == nil
+        {
+            return 0
+        }
+        else
+        {
+            return zdarzenia!.count
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: UITableViewCell.CellStyle.subtitle, reuseIdentifier: "cell")
+        cell.detailTextLabel?.text = self.zdarzenia![indexPath.item].nazwa + " " + self.zdarzenia![indexPath.item].jednostkaNazwa
+        cell.textLabel?.text = self.zdarzenia![indexPath.item].czas
+        return cell
+    }
+    
     func showToast(message : String) {
         
         let toastLabel = UILabel(frame: CGRect(x: self.view.frame.size.width/2 - 75, y: self.view.frame.size.height-100, width: 150, height: 35))
